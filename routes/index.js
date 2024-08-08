d0a2b22e77e8057f23cf961dff0e77286092f0b3
RandomGod <63667312+Prince453-hack@users.noreply.github.com>
2024-08-08 13:01:11 +0530

    completed

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import articles from "../data/articles.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Listing", articles });
+});
+
+router.get("/articles/:id", (req, res) => {
+  const article = articles.find((a) => a.id === parseInt(req.params.id));
+  res.render("article", { title: article.name, article: article });
+});
+
+router.post("/articles", (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000) + 1,
+    name,
+    body,
+  };
+
+  articles.push(article);
+
+  setTimeout(() => {
+    res.render("partials/list", { articles });
+  }, 3000);
+});
+
+export default router;
